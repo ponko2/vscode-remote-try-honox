01803525a6c7bdd3e33804640acc155b4cada7ac
Takahito Nakano <kano@ponko2.jp>
2024-04-29 22:49:22 +0900

    Initial commit

---
+++ app/global.d.ts
@@ -0,0 +1,21 @@
+import {} from "hono";
+
+type Head = {
+  title?: string;
+};
+
+declare module "hono" {
+  interface Env {
+    // biome-ignore lint/complexity/noBannedTypes: <explanation>
+    Variables: {};
+    // biome-ignore lint/complexity/noBannedTypes: <explanation>
+    Bindings: {};
+  }
+  interface ContextRenderer {
+    // biome-ignore lint/style/useShorthandFunctionType: <explanation>
+    (
+      content: string | Promise<string>,
+      head?: Head,
+    ): Response | Promise<Response>;
+  }
+}
