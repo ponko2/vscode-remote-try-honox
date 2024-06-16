8f3d81566a4550087877aadef2c315c232baee9f
Takahito Nakano <kano@ponko2.jp>
2024-06-17 01:29:42 +0900

    build: Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,21 @@
 import pages from "@hono/vite-cloudflare-pages";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import honox from "honox/vite";
-import client from "honox/vite/client";
-import { type SSROptions, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
-const ssr = {
-  external: ["@prisma/client"],
-} satisfies SSROptions;
-
-export default defineConfig(({ mode }) => {
-  if (mode === "client") {
-    return {
-      ssr,
-      build: {
-        rollupOptions: {
-          input: ["/app/style.css"],
-        },
+export default defineConfig({
+  ssr: {
+    external: ["@prisma/client"],
+  },
+  plugins: [
+    honox({
+      client: {
+        input: ["/app/style.css"],
       },
-      plugins: [client()],
-    };
-  }
-  return {
-    ssr,
-    plugins: [
-      honox({
-        devServer: {
-          adapter,
-        },
-      }),
-      pages(),
-    ],
-  };
+      devServer: {
+        adapter,
+      },
+    }),
+    pages(),
+  ],
 });
