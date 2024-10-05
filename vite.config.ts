26b98c53624410d394c74b81324bf7b18f30730d
Takahito Nakano <kano@ponko2.jp>
2024-10-05 21:39:44 +0900

    build: Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import pages from "@hono/vite-cloudflare-pages";
+import build from "@hono/vite-build/cloudflare-pages";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
@@ -16,6 +16,6 @@ export default defineConfig({
         adapter,
       },
     }),
-    pages(),
+    build(),
   ],
 });
