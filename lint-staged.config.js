934e0b71f690389e459ee7c9c49b67784cf6fba0
Takahito Nakano <kano@ponko2.jp>
2025-07-18 20:31:52 +0900

    chore: Update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@
  */
 export default {
   "*.{js,jsx,ts,tsx,cjs,cts,mjs,mts,json,jsonc,css,svelte,vue,astro,graphql,gql}":
-    "biome check --write --no-errors-on-unmatched",
+    "biome check --write --no-errors-on-unmatched --error-on-warnings",
   "schema.prisma": (filenames) =>
     filenames.flatMap((filename) => [
       `prisma format --schema=${filename}`,
